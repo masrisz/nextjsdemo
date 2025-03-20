a96f54833c2ab7db7adff10c5653a7c1bbc4d3c5
masrisz <vlogcod3r@gmail.com>
2025-03-20 11:08:37 +0700

    add basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || "", // Jika env kosong, basePath tidak digunakan
 };
 
 export default nextConfig;
